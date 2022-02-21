1fcc7e6a07d6c9b3bac61fb70274d04750cc2f76
ChrisOnsando <chrisnemwel@gmail.com>
2022-02-21 03:57:10 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+first_name = "Chris"
+last_name = "Onsando"
+full_name = first_name + last_name
+
+age = 19
+age += 1
+
+height = 250.20
+
+human = True
+
+school = 'university'
+
+residence = 'Nairobi'
+
+profession = 'Software Development'
+
+hobbies ='Table Tennis, Swimming, Watching Documentaries'
+
+print("My names " +full_name)
+print("My age is: "+str(age))
+print("My height is: "+str(height)+"cm")
+print("Are you human: "+str(human))
+print("Am currently in the "+str(school))
+print("Am currently living in "+str(residence))
+print("Am currently in the profession of "+str(profession))
+print("Currently my hobbies are: "+str(hobbies))
+print(type(age))
+print(type(height))
+print(type(human))
+print(type(school))
+print(type(residence))
+print(type(profession))
+print(type(hobbies))
