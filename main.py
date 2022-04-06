e65cd2b6269bdecb8265df6210de9a03e18c21c4
Chris Onsando <chrisnemwel@gmail.com>
2022-04-06 03:13:40 -0600

    fourth commit

--- main.py
+++ main.py
@@ -225,3 +225,6 @@ itemno = 567
 price = 49.95
 myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
 print(myorder.format(quantity, itemno, price)) 
+
+#Escape characters
+txt = "We are the so-called \"Vikings of Valhalla\" from the north."
